67fa6dc326c987d6599ae5bf6e0bb068b06ceca2
Jason Awbrey and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-02 10:40:08 -0800

    Add red stage tests for save, getAll, deleteAll methods for Author class

---
+++ tests/AuthorTest.php
@@ -0,0 +1,73 @@
+<?php
+
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
+
+	$server = "mysql:host=localhost;dbname=library_test";
+	$username = "root";
+	$password = "root";
+	$DB = new PDO($server, $username, $password);
+
+	class AuthorTest extends PHPUnit_Framework_TestCase
+	{
+		protected function tearDown()
+		{
+			Book::deleteAll();
+			Author::deleteAll();
+			Author::deleteAll();
+		}
+
+		function test_save()
+		{
+			//Arrange
+			$name = "Jason Awbrey";
+			$test_author = new Book($name);
+			$test_author->save();
+
+			//Act
+			$result = Author::getAll();
+
+			//Assert
+			$this->assertEquals([$test_author], $result);
+		}
+		function test_getAll()
+		{
+			//Arrange
+			$name = "Jason Awbrey";
+			$test_author = new Author($name);
+			$test_author->save();
+
+			$name = "Yvonna Contreras";
+			$test_author2 = new Author($name);
+			$test_author2->save();
+
+			//Act
+			$result = Author::getAll();
+
+			//Assert
+			$this->assertEquals([$test_author, $test_author2], $result);
+		}
+
+		function test_deleteAll()
+		{
+			//Arrange
+			$name = "Jason Awbrey";
+			$test_author = new Author($name);
+			$test_author->save();
+
+			$name = "Yvonna Contreras";
+			$test_author2 = new Author($name);
+			$test_author2->save();
+
+			//Act
+			Author::deleteAll();
+			$result = Author::getAll();
+
+			//Assert
+			$this->assertEquals([], $result);
+		}
+	}
+
+?>
