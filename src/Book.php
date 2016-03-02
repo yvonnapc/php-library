0be8d005ecc74b9c2eb3fd9389a9be62fb677a18
Jason Awbrey and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-02 10:10:16 -0800

    Remove author argument of Book class

--- src/Book.php
+++ src/Book.php
@@ -2,13 +2,11 @@
 	 class Book
 		{
 		private $title;
-		private $author;
 		private $id;
 
-		function __construct($title, $author, $id = NULL)
+		function __construct($title, $id = NULL)
 		{
 			$this->title = $title;
-			$this->author = $author;
 			$this->id = $id;
 		}
 
@@ -20,16 +18,6 @@
 		function setTitle($title)
 		{
 			$this->title = $title;
-		}
-
-		function getAuthor()
-		{
-			return $this->author;
-		}
-
-		function setAuthor($author)
-		{
-			$this->author = $author;
 		}
 
 		function getId()
--- tests/BookTest_.php
+++ tests/BookTest_.php
@@ -1,16 +1,29 @@
 <?php
 
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
+
+	$server = "mysql:host=localhost;dbname=library_test";
+	$username = "root";
+	$password = "root";
+	$DB = new PDO($server, $username, $password);
+
 	class BookTest extends PHPUnit_Framework_TestCase
 	{
-		function test_getTitle()
+		function test_save()
 		{
 		//Arrange
 		$title = "Cathedral";
-		$test_book = new Book($title, $author);
+		$test_book = new Book($title);
+		$test_book->save();
+
 		//Act
-		$result = $test_book->getTitle($title);
+		$result = Book::getAll();
+		
 		//Assert
-		$this->assertEquals('Cathedral', $result);
+		$this->assertEquals([$test_book], $result);
 		}
 	}
 
